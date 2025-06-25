0e4d460f3661ce9b261681df2b1a4a4ef37c021b
alvin-alex <alpinoi.alex@gmail.com>
2025-06-25 15:18:07 +1000

    Setup Clash Display Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['ClashDisplay-Variable', 'Inter', 'system-ui', 'sans-serif'],
       },
       colors: {
         primary: {
